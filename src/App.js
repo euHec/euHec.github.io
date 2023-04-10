3b1d94ac799cd9fb0a499749e3cbb2f2afeb59aa
euHec <hectorsouzass@gmail.com>
2023-04-10 02:16:07 -0300

    Atualização de "homepage" no package.json

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-// import { Route, Routes } from 'react-router-dom';
-// import './App.css';
-// import Home from './pages/Home';
-// import Bio from './pages/Bio';
-// import Contato from './pages/Contato';
-// import Portifolio from './pages/Portifolio';
+import { Route, Routes as Switch } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Bio from './pages/Bio';
+import Contato from './pages/Contato';
+import Portifolio from './pages/Portifolio';
 
-// function App() {
-//   return (
+function App() {
+  return (
+    <Switch>
+      <Route exact element={ <Home /> } path="/" />
+      <Route exact element={ <Bio /> } path="/bio" />
+      <Route exact element={ <Contato /> } path="/contato" />
+      <Route exact element={ <Portifolio /> } path="/portifolio" />
+  </Switch>
+  );
+}
 
-//   );
-// }
-
-// export default App;
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes as Switch } from 'react-router-dom';
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
-// import App from './App';
-import Home from './pages/Home';
-import Bio from './pages/Bio';
-import Contato from './pages/Contato';
-import Portifolio from './pages/Portifolio';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Switch>
-        <Route exact element={ <Home /> } path="/" />
-        <Route exact element={ <Bio /> } path="/bio" />
-        <Route exact element={ <Contato /> } path="/contato" />
-        <Route exact element={ <Portifolio /> } path="/portifolio" />
-      </Switch>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 );
