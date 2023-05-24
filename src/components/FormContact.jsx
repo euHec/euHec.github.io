869d364dccf44e1f69093df93f9ac5e8ac9ddf68
euHec <hectorsouzass@gmail.com>
2023-05-24 12:54:50 -0300

    Implementa serviço para enviar e-mail quando os dados foram enviados

--- src/components/FormContact.jsx
+++ src/components/FormContact.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import emailjs from '@emailjs/browser';
 import '../styles/FormContact.css'
 import formImg from '../imagens/form.svg'
 
@@ -17,18 +18,32 @@ export default function FormContact() {
     setData((prev) => ({ ...prev, [target.name]: value }))
   }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const newEmail = await emailjs.send('service_ctfu1jp', 'template_dkid50a', data, 'LZWdwlugSwp9YWOcv')
+      if (newEmail.status !== 200) throw new Error(newEmail.text)
+
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setData(stateInitial);
+    }
+  }
+
   return (
     <div className="area-form">
       <div>
         <h1 className="title-form" >Solicitar contato</h1>
         <img src={formImg} alt="svg"/>
       </div>
-      <form action="https://formsubmit.co/hectorsouzass@gmail.com" method='POST'>
+      <form onSubmit={ handleSubmit }>
         <input type="text" placeholder='digite seu nome' value={ data.name } name='name'onChange={ handleChanges }/>
         <input type="email" placeholder='exemple@email.com' value={ data.email } name='email'onChange={ handleChanges }/>
         <input type="number" placeholder='digite seu número' value={ data.number } name='number'onChange={ handleChanges }/>
         <label htmlFor="checked"><input type="checkbox" checked={ data.agreed } name='agreed' id="checked" onChange={ handleChanges }/>O número informado é WhatsApp</label>
-        <textarea name="message" cols="30" rows="10" placeholder='Deixe a sua mensagem' onChange={ handleChanges }/>
+        <textarea name="message" cols="30" rows="10" placeholder='Deixe a sua mensagem' value={ data.message } onChange={ handleChanges }/>
         <button type="submit">Solicitar contato</button>
       </form>
     </div>
