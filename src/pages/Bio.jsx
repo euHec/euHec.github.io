41fc7ad9f7d72ffe01f21286e59af153492d6fcd
euHec <hectorsouzass@gmail.com>
2023-05-24 11:19:06 -0300

    Altera página de bio para componente funcional

--- src/pages/Bio.jsx
+++ src/pages/Bio.jsx
@@ -1,13 +1,10 @@
-import { Component } from 'react';
 import Header from '../components/Header';
 
-export default class Bio extends Component {
-  render() {
+export default function Bio () {
     return (
       <>
         <Header />
         <h1>Bio</h1>
       </>
     );
-  }
 }
