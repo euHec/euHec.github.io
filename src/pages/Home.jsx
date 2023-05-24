b3c93711fcb0a561ec3f5426fca2a03f5cf09598
euHec <hectorsouzass@gmail.com>
2023-05-24 11:21:20 -0300

    Altera página de Home para componente funcional

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import { FaInstagram, FaWhatsapp, FaLinkedin, FaGithub } from 'react-icons/fa';
 import MyPhoto from '../imagens/my photo.PNG';
 import '../styles/Home.css';
@@ -6,46 +5,44 @@ import Icon from '../components/Icon';
 import Header from '../components/Header';
 
 
-export default class Home extends Component {
-  render() {
-    return (
-      <>
-        <Header />
-        <main className="content-home">
-          <div className="data">
-            <div className="data-person">
-              <h1>Hector Souza</h1>
-              <h2>FrontEnd Developer</h2>
-            </div>
-            <div className="social">
-              <h3>@_euHec</h3>
-              <Icon
-                link="https://www.instagram.com/_euhec/"
-                label="instagram"
-                icon={ <FaInstagram size="30px" /> }
-              />
-              <Icon
-                link="https://www.instagram.com/_euhec/"
-                label="instagram"
-                icon={ <FaLinkedin size="30px" /> }
-              />
-              <Icon
-                link="https://wa.me/5575991821105/"
-                label="WhatsApp"
-                icon={ <FaWhatsapp size="30px" /> }
-              />
-              <Icon
-                link="https://github.com/euHec"
-                label="Github"
-                icon={ <FaGithub size="30px" /> }
-              />
-            </div>
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main className="content-home">
+        <div className="data">
+          <div className="data-person">
+            <h1>Hector Souza</h1>
+            <h2>FrontEnd Developer</h2>
           </div>
-          <div className="photo">
-            <img src={ MyPhoto } alt="Hector Souza" />
+          <div className="social">
+            <h3>@_euHec</h3>
+            <Icon
+              link="https://www.instagram.com/_euhec/"
+              label="instagram"
+              icon={ <FaInstagram size="30px" /> }
+            />
+            <Icon
+              link="https://www.instagram.com/_euhec/"
+              label="instagram"
+              icon={ <FaLinkedin size="30px" /> }
+            />
+            <Icon
+              link="https://wa.me/5575991821105/"
+              label="WhatsApp"
+              icon={ <FaWhatsapp size="30px" /> }
+            />
+            <Icon
+              link="https://github.com/euHec"
+              label="Github"
+              icon={ <FaGithub size="30px" /> }
+            />
           </div>
-        </main>
-      </>
-    );
-  }
+        </div>
+        <div className="photo">
+          <img src={ MyPhoto } alt="Hector Souza" />
+        </div>
+      </main>
+    </>
+  );
 }
