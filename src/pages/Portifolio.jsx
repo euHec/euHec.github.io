12b848167f707e4e27d04fb8c32b3d9eb55944d1
euHec <hectorsouzass@gmail.com>
2023-05-24 21:47:13 -0300

    Implementa renderização dos cards na página de potifólio

--- src/pages/Portifolio.jsx
+++ src/pages/Portifolio.jsx
@@ -1,4 +1,6 @@
+import CardProjects from '../components/CardProjects';
 import Header from '../components/Header';
+import projectsData from '../projetos/projectsData';
 
 export default function Portifolio() {
   return (
@@ -6,7 +8,18 @@ export default function Portifolio() {
       <Header />
       <main>
         <h1>Portifolio</h1>
-
+        {
+          projectsData.forEach((projects) => {
+            <CardProjects
+              name = { projects.name }
+              repository = { projects.repository }
+              deploy = { projects.deploy }
+              thumbnails = { projects.thumbnails }
+              description = { projects.description }
+              technologies = { projects.technologies }
+            />
+          })
+        }
       </main>
     </>
   );
