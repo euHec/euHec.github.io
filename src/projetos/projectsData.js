54ca0d8681f7ffa841c715819f4667b836945e2b
euHec <hectorsouzass@gmail.com>
2023-05-24 21:42:53 -0300

    Adiciona objeto parcial com os projetos

---
+++ src/projetos/projectsData.js
@@ -0,0 +1,44 @@
+const projectsData = [
+  {
+    name: 'TrybeTunes',
+    repository: 'https://github.com/euHec/trybe_tunes',
+    deploy: 'https://euhec.github.io/trybe_tunes/',
+    thumbnails: '',
+    description: '',
+    technologies: ['REACT.JS', 'CSS','REDUX'],
+  },
+  {
+    name: 'TrybeWallet',
+    repository: 'https://github.com/euHec/trybe-wallet',
+    deploy: 'https://euhec.github.io/trybe-wallet/',
+    thumbnails: '',
+    description: '',
+    technologies: ['REACT.JS', 'CSS', 'REDUX'],
+  },
+  {
+    name: 'App de receitas',
+    repository: 'https://github.com/euHec/recipes_app',
+    deploy: 'https://euhec.github.io/recipes_app/',
+    thumbnails: '',
+    description: '',
+    technologies: ['REACT.JS', 'CSS', 'TAILWINDCSS', 'CONTEXT API'],
+  },
+  {
+    name: 'Meu Redirect',
+    repository: 'https://github.com/euHec/meu-redirect',
+    deploy: 'https://euhec.github.io/meu-redirect/',
+    thumbnails: '',
+    description: '',
+    technologies: ['REACT.JS', 'CSS', 'CONTEXT API'],
+  },
+  {
+    name: 'Movimento Esperançar',
+    repository: 'https://github.com/euHec/projeto_esperancar',
+    deploy: 'https://euhec.github.io/projeto_esperancar/',
+    thumbnails: '',
+    description: '',
+    technologies: ['HTML', 'CSS', 'JAVASCRIPT', 'TAILWINDCSS'],
+  }
+]
+
+export default projectsData;
