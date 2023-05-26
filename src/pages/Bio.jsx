061bc1fb471726744081422f616f02190fb4d228
euHec <hectorsouzass@gmail.com>
2023-05-26 13:01:54 -0300

    Finaliza contrução e estilização da página de Bio

--- src/pages/Bio.jsx
+++ src/pages/Bio.jsx
@@ -1,8 +1,46 @@
+import study from '../imagens/study.svg'
+import eu from '../imagens/eu.jpeg'
+import { useContext } from 'react';
+import { Context } from '../context/Context';
 
 export default function Bio () {
+  const { theme } = useContext(Context);
+
     return (
-      <section className="pt-20">
-        <h1>Bio</h1>
-      </section>
-    );
+    <section className="pt-20 px-5">
+      <h1 className="text-center text-6xl py-5">Sobre mim!</h1>
+      <div className="w-full items-center justify-center flex gap-10">
+        <div className="w-1/3 text-justify">
+          <h3>Seja bem-vindo! Meu nome é Hector Souza e sou natural de Feira de Santana, na Bahia. Sempre tive paixão por tecnologia e, após explorar diferentes áreas, decidi fazer uma transição de carreira e me tornar um desenvolvedor. Atualmente, meu foco está no desenvolvimento <strong>Front-end</strong>, mas também estou me especializando em <strong>Back-end</strong>.</h3>
+          <br />
+          <h3>Durante minha trajetória profissional, tive a oportunidade de trabalhar em empresas como Huggy, Fresh Filmes, Conecta Minas e Rank My App. Nessas experiências, desempenhei funções relacionadas a vendas, atendimento e cobrança. Em algumas delas, tive a chance de ter um contato mais próximo com a tecnologia, o que apenas reforçou minha paixão pela área.</h3>
+          <div className="w-full text-right">
+            <a href="https://www.linkedin.com/in/hectorsouza/" target="_blank" rel="noreferrer" className="text-xs text-end hover:text-slate-500">clique para saber mais sobre carreira...</a>
+          </div>
+        </div>
+        <img src={ eu } alt="formação" className="w-2/6 rounded-full"/>
+      </div>
+      <div className="w-full items-center justify-center flex">
+        <img src={ study } alt="formação" className="w-2/6"/>
+        <div className="w-2/6">
+
+          <h1 className="text-2xl text-center py-3">Formação</h1>
+          <div className="py-3">
+            <h2 className={`p-1 ${theme === 'ligth' ? 'bg-black text-white' : 'bg-white text-black' } text-center rounded-xl w-44`}>Nov/2022 - Nov/2023</h2>
+            <h3 className="pl-5">Desenvolvimento Web Full Stack - <strong><a href="betrybe.com" target="_blank">Trybe</a></strong></h3>
+          </div>
+
+          <div className="py-3">
+            <h2 className={`p-1 ${theme === 'ligth' ? 'bg-black text-white' : 'bg-white text-black' } text-center rounded-xl w-44`}>Jun/2019 - Em pausa</h2>
+            <h3 className="pl-5">Sistemas de informação - UNIFTC</h3>
+          </div>
+
+          <div className="py-3">
+            <h2 className={`p-1 ${theme === 'ligth' ? 'bg-black text-white' : 'bg-white text-black' } text-center rounded-xl w-44`}>Jun/2016 - Jun/2018</h2>
+            <h3 className="pl-5">Técnico em Mecânica, SENAI</h3>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
